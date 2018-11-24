5a78ea35729d0ef8cca84f56fa7ff22ad8bb2b94
Iulian <iulian.moisa@gmail.com>
2018-11-24 14:30:37 -0800

    included the react client build

---
+++ src/main/resources/static/precache-manifest.61290d9b28980e6245d842e6c6a5d0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93cea08c96d4b314f3b3",
+    "url": "/static/js/main.93cea08c.chunk.js"
+  },
+  {
+    "revision": "386abe3b845c7c35ffd0",
+    "url": "/static/js/1.386abe3b.chunk.js"
+  },
+  {
+    "revision": "93cea08c96d4b314f3b3",
+    "url": "/static/css/main.7d8f8918.chunk.css"
+  },
+  {
+    "revision": "386abe3b845c7c35ffd0",
+    "url": "/static/css/1.c12548bb.chunk.css"
+  },
+  {
+    "revision": "9ea1d69008ac177da1feaa16af197d77",
+    "url": "/index.html"
+  }
+];
